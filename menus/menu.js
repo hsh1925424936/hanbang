5a2e21ba74de3df1dea980f9ea27e01bd8052ffb
hushihao <1925424936@qq.com>
2018-12-13 17:17:39 +0800

    2018-12-13

---
+++ menus/menu.js
@@ -0,0 +1,36 @@
+;(function($){
+    $.fn.menu = function(options){
+        var defaults = {
+            eventType1:options.eventType1 || 'mouseover',
+            eventType2:options.eventType2 || 'mouseout',
+            data:options.data || []
+        }
+        this.each(function(){
+            var $this = $(this);
+            var $ul = $('<ul class="menuUl"></ul>');
+            $this.append($ul);
+            renderList($ul,defaults.data);
+        })
+        function renderList($dom,data){
+            $.each(data,function(index,item){
+                var $li = $('<li class="menuLi"></li>');
+                var $span = $('<span class="menuSpan">'+item.title+'</span>');
+                var $div = $('<div class="menuDiv"><ul class="menuUl"></ul></div>');
+                $li.append($span);
+                if(item.children && item.children.length>0){
+                    $li.append($div);
+                    renderList($div.find('.menuUl'),item.children);
+                }else{
+                    $li.addClass('no-data');
+                }
+                $dom.append($li);
+            });
+            $dom.children('.menuLi').bind(defaults.eventType1,function(event){
+                $(this).children('.menuDiv').show();
+            })
+            $dom.children('.menuLi').bind(defaults.eventType2,function(event){
+                $(this).children('.menuDiv').hide();
+            })
+        }
+    }
+})(jQuery);
